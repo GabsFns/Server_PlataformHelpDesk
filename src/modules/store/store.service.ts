002939968fe9575b30de527a773e265c6906a836
GabsFns <gabriel0917py@gmail.com>
2025-08-07 16:56:26 -0300

    fix: modificando status chamado e melhorando a funcionalidade, interligando tabela anexo, status desejados

--- src/modules/store/store.service.ts
+++ src/modules/store/store.service.ts
@@ -2,31 +2,43 @@ import { FastifyInstance } from "fastify";
 import { enviarChamadoParaFila } from "@/services/rabbitmqService";
 import { io } from "../../websocket/index";
 import { CreateChamadoInput } from "./store.schema";
-export async function createChamado(fastify: FastifyInstance, data: CreateChamadoInput) {
-    const chamado = await fastify.prisma.chamado.create({
-      data: {
-        titulo: data.titulo,
-        descricao: data.descricao,
-        tipo: data.tipo,
-        status: data.status,
-        dataConclusao: data.dataConclusao ? new Date(data.dataConclusao) : null,
-        prioridade: data.prioridade,
-        lojaId: data.lojaId,
-        setorId: data.setorId,
-        tokenId: data.tokenId,
-       
-      },
-      include: {
-        loja: true,
-        setor: true,
-      },
-    });
-    
-    await enviarChamadoParaFila(chamado);
-    const routingKey = chamado.setor.nome.toUpperCase();
-    io.to(routingKey).emit('novoChamado', chamado);
-    console.log(`Evento 'novoChamado' emitido para o room ${routingKey}`);
-    
-     return chamado;
-}
+export async function createChamado(
+  fastify: FastifyInstance,
+  data: CreateChamadoInput
+) {
+  const chamado = await fastify.prisma.chamado.create({
+    data: {
+      titulo: data.titulo,
+      descricao: data.descricao,
+      tipo: data.tipo,
+      //status: data.status,
+      dataConclusao: data.dataConclusao ? new Date(data.dataConclusao) : null,
+      prioridade: data.prioridade,
+      lojaId: data.lojaId,
+      setorId: data.setorId,
+      tokenId: data.tokenId,
 
+      anexos: {
+        create:
+          data.anexos?.map((anexo) => ({
+            arquivo: anexo.arquivo,
+            lojaId: data.lojaId,
+            setorId: data.setorId,
+            tokenId: data.tokenId,
+          })) || [],
+      },
+    },
+    include: {
+      loja: true,
+      setor: true,
+      anexos: true,
+    },
+  });
+
+  await enviarChamadoParaFila(chamado);
+  const routingKey = chamado.setor.nome.toUpperCase();
+  io.to(routingKey).emit("novoChamado", chamado);
+  console.log(`Evento 'novoChamado' emitido para o room ${routingKey}`);
+
+  return chamado;
+}
